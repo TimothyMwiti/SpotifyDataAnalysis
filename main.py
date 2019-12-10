0f97a8cecd2b402bff15d8acfffbc58d781f4b29
Timothy Mwiti <mwititimothyk@gmail.com>
2019-12-09 20:11:44 -0500

    Retrieving most popular artist from streaming history implemented

--- main.py
+++ main.py
@@ -1,22 +1,18 @@
 #!/usr/bin/env python3
-
+# 
 import json
 import os
 
 FILE_NAME = "StreamingHistory1.json"
 
+# TODO: If two or more artists can share the same artist name:
+#           change logic
+
 def get_most_popular_artist_from_streaming_history(
     base_dir="./",
     streaming_files_prefix="StreamingHistory"):
 
-    streaming_history = []
-
-    for file in os.listdir(base_dir):
-        if 'json' in file and streaming_files_prefix in file:
-            json_path = os.path.join(base_dir, file)
-            
-            with open(json_path, 'r') as f:
-                streaming_history = streaming_history + json.load(f)
+    streaming_history = read_json_data_from_files_in_common_folder_by_prefix(base_dir, streaming_files_prefix)    
 
     if len(streaming_history) < 1:
         raise DataNotFound("Unable to read data from streaming history files")
@@ -32,13 +28,27 @@ def get_most_popular_artist_from_streaming_history(
             artist_streams_count[artistName] = 1
             
 
-    most_popular_artist = next(iter(artist_streams_count)) 
+    most_popular_artist = next(iter(artist_streams_count))
+    most_popular_artist_count = artist_streams_count[most_popular_artist]
 
-    for key in artist_streams_count:
-        if artist_streams_count[key] > artist_streams_count[most_popular_artist]:
-            most_popular_artist = key
+    for artist_name, artist_stream_count in artist_streams_count.items():
+        if artist_stream_count > most_popular_artist_count:
+            most_popular_artist = artist_name
+            most_popular_artist_count = artist_streams_count[most_popular_artist]
     
     return most_popular_artist
+
+def read_json_data_from_files_in_common_folder_by_prefix(base_dir, prefix):
+    data = []
+
+    for file in os.listdir(base_dir):
+        if 'json' in file and prefix in file:
+            json_path = os.path.join(base_dir, file)
+            
+            with open(json_path, 'r') as f:
+                data = data + json.load(f)
+    
+    return data
 
 class DataNotFound(Exception):
     def __init__(self, message):

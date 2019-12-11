b6d59c0d8e33466d97e28a573ea94c37f74dcb13
Timothy Mwiti <mwititimothyk@gmail.com>
2019-12-11 13:52:38 -0500

    refactored code to avoid code duplication in checking if artist in streaming record

--- main.py
+++ main.py
@@ -2,14 +2,13 @@
  
 import json
 import os
-
 from utilities import *
-# TODO: If two or more artists can share the same artist name:
-#           change logic
 
 STREAMING_HISTORY = read_json_data_from_files_in_common_folder_by_prefix("./", "StreamingHistory")
 MILLISECONDS_IN_A_MINUTE = 60000
 
+# TODO: If two or more artists can share the same artist name:
+#           change logic
 def get_most_popular_artist_from_streaming_history(
     base_dir="./",
     streaming_files_prefix="StreamingHistory"):
@@ -47,7 +46,7 @@ def get_most_popular_artist_from_streaming_history(
     
     return most_popular_artist
 
-def get_number_of_times_listened_to_artist_by_name(
+def get_number_of_times_listened_to_artist(
     artist_name="",
     base_dir="./",
     streaming_files_prefix="StreamingHistory"):
@@ -59,14 +58,11 @@ def get_number_of_times_listened_to_artist_by_name(
 
     artist_stream_count = 0
 
-    for i in range(0, len(streaming_history)):
-        if streaming_history[i]["artistName"] == artist_name or \
-            artist_name in streaming_history[i]["trackName"]:
+    for streaming_record in streaming_history:
+        if artist_appears_in_streaming_record(artist_name, streaming_record):
             artist_stream_count = artist_stream_count + 1
-    
 
     return artist_stream_count
-
 
 def get_number_of_minutes_listened_to_artist(
     artist_name="",
@@ -81,56 +77,18 @@ def get_number_of_minutes_listened_to_artist(
     milliseconds_listened_to_artist = 0
 
     for streaming_record in streaming_history:
-        if streaming_record["artistName"] == artist_name or \
-            artist_name in streaming_record["trackName"]:
-            print(str(streaming_record) + "\n\n")
+        if artist_appears_in_streaming_record(artist_name, streaming_record):
             milliseconds_listened_to_artist = milliseconds_listened_to_artist + streaming_record["msPlayed"]
     
     return milliseconds_listened_to_artist // MILLISECONDS_IN_A_MINUTE
 
-# TODO: Query against API to establish that names in results are artist names
-def get_list_of_featured_artists_from_track_name(
-    track_name=""):
+def artist_appears_in_streaming_record(artist_name, streaming_record):
+    if streaming_record["artistName"] == artist_name or \
+            artist_name in streaming_record["trackName"]:
+        return True
+    return False
 
-    if len(track_name) < 1 or ("(" not in track_name and "[" not in track_name):
-        return []
-    
-    featured_artists = []
 
-    if "feat" in track_name:
-        featured_artists_str = track_name.split("(")[1][:-1][6:]
-    elif "with" in track_name:
-        featured_artists_str = track_name.split("with")[-1].strip()
-        if ")" == featured_artists_str[-1] or "]" == featured_artists_str[-1]:
-            featured_artists_str = featured_artists_str[:-1].strip()
-    else:
-        featured_artists_str = track_name.split("(")[-1][:-1]
-    
-    featured_artists = get_artists_from_string_with_multiple_artists(featured_artists_str)
-
-    return featured_artists
-
-def get_artists_from_string_with_multiple_artists(featured_artists_str):
-    featured_artists = []
-    
-    if "&" in featured_artists_str:
-        temp_artists_split = featured_artists_str.split("&")
-        featured_artists.append(temp_artists_split[-1].strip())
-
-        featured_artists_str = temp_artists_split[0]
-
-        if "," not in featured_artists_str:
-            featured_artists.append(featured_artists_str.strip())
-        else:
-            artists = featured_artists_str.split(",")
-
-            for artist in artists:
-                featured_artists.append(artist.strip())
-    else:
-        featured_artists = [featured_artists_str]
-    
-    return featured_artists    
-    
 class DataNotFound(Exception):
     def __init__(self, message):
         self.message = message
@@ -139,5 +97,5 @@ class DataNotFound(Exception):
 if __name__ == "__main__":
     print("The most popular artist is:", get_most_popular_artist_from_streaming_history())
     print("The number of times you played Drake is:",
-        get_number_of_times_listened_to_artist_by_name("Drake"))
+        get_number_of_times_listened_to_artist("Drake"))
     print("You listened to Drake for {} minutes.".format(get_number_of_minutes_listened_to_artist("Drake")))

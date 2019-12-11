2279d4819996ad94166952898f3c0ed9e671ccd7
Timothy Mwiti <mwititimothyk@gmail.com>
2019-12-11 11:58:04 -0500

    Searching for artists featured on songs and addition of unit tests.
    Implemented search of artists based on credits on track name.
    This covered some edge cases.
    Introduced TDD in the project through unit tests.

---
+++ utilities_test.py
@@ -0,0 +1,27 @@
+import pytest
+from main import get_list_of_featured_artists_from_track_name
+
+class TestGettingFeaturedArtistsFromTrackName:
+
+    def test_track_contains_no_featured_artist(self):
+        assert [] == get_list_of_featured_artists_from_track_name("Who Want The Smoke?")
+        
+    
+    def test_track_contains_one_featured_artist_concated_using_feat(self):
+        assert ["Drake"] == get_list_of_featured_artists_from_track_name("Going Bad (feat. Drake)")
+    
+
+    def test_track_contains_multiple_featured_artist_concated_using_feat(self):
+        assert ["Offset", "Travis Scott"] == sorted(get_list_of_featured_artists_from_track_name("ZEZE (feat. Travis Scott & Offset)"))
+        assert ["A$AP Rocky", "French Montana", "ScHoolboy Q", "Trinidad James"] == sorted(get_list_of_featured_artists_from_track_name(
+            "Work REMIX (feat. A$AP Rocky, French Montana, Trinidad James & ScHoolboy Q)"
+        ))
+        
+
+    def test_track_contains_multiple_featured_artist_concated_using_with(self):
+        assert ["Keala Settle", "Kesha", "Missy Elliott"] == \
+            sorted(get_list_of_featured_artists_from_track_name(
+                "This Is Me (The Reimagined Remix) [with Keala Settle, Kesha & Missy Elliott]"))
+
+    def test_track_contains_multipled_featured_artist_concated_with_parentheses(self):
+        assert ["Gunna", "Lil Baby"] == sorted(get_list_of_featured_artists_from_track_name("Drip Too Hard (Lil Baby & Gunna)"))
